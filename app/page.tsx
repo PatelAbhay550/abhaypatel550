2a3c98dc023d01555006b33503cecd8067e1d6c5
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2026-05-18 09:25:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import BackgroundWave from './components/Wave';
 
 export default function Home() {
   const abhayData = {
-    image: "https://scontent.fknu1-4.fna.fbcdn.net/v/t39.30808-6/473134353_1797331867702105_3172409557775362407_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=9F4x1-wiGlsQ7kNvwELPEF5&_nc_oc=AdnGD2pfLPbdvqm3AfI4c_venoSwpWTB958qT-z_SGvcgvsbRap2E8Spm1k4VEWajf8&_nc_zt=23&_nc_ht=scontent.fknu1-4.fna&_nc_gid=IE3IqhWUMBEpgP86IewrWw&oh=00_AfqpFpcdEN9RThRyYyA1D08720pKLu8GrO18csHMTzjRZQ&oe=697E2066",
+    image: "https://avatars.githubusercontent.com/u/90134639?v=40_tt6&_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=9F4x1-wiGlsQ7kNvwELPEF5&_nc_oc=AdnGD2pfLPbdvqm3AfI4c_venoSwpWTB958qT-z_SGvcgvsbRap2E8Spm1k4VEWajf8&_nc_zt=23&_nc_ht=scontent.fknu1-4.fna&_nc_gid=IE3IqhWUMBEpgP86IewrWw&oh=00_AfqpFpcdEN9RThRyYyA1D08720pKLu8GrO18csHMTzjRZQ&oe=697E2066",
     title: "Developer",
     name: "Abhay Patel",
     description: "I am a frontend developer with 5 years of experience building web applications, specializing in Next.js and modern React ecosystems."
