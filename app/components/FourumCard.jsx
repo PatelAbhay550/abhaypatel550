9031feafac84ebe42e0b30f7cb908be8594b87cb
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2026-05-18 09:51:03 +0530

    Update user image source in FourumCard component

--- app/components/FourumCard.jsx
+++ app/components/FourumCard.jsx
@@ -38,7 +38,7 @@ const FourumCard = ({ projects, onHover, onLeave }) => {
                 height={40}
                 width={40}
                 alt="User"
-                src="https://scontent.fknu1-6.fna.fbcdn.net/v/t39.30808-1/334998150_2915813855229526_2877140648363160727_n.jpg?stp=c74.0.790.790a_dst-jpg_s200x200_tt6&_nc_cat=102&ccb=1-7&_nc_sid=e99d92&_nc_ohc=98eOaJc3IG0Q7kNvwEXIXGG&_nc_oc=AdnJ0KvCLq_iSQaCG4fMC0yhWMoFIROUq9yaw8n-TTyZT8fJmtz3eyzYWE5uAJWI-pE&_nc_zt=24&_nc_ht=scontent.fknu1-6.fna&_nc_gid=IE3IqhWUMBEpgP86IewrWw&oh=00_AfoVkF9Ja5nhaqMX67heagOlg0c-GZ-20xDLTBt8sj70wA&oe=697E2A45"
+                src="https://ibb.co/Q3xC8gnz"
                 className="size-10 rounded-xl object-cover ring-2 ring-gray-50"
               />
               <span className="absolute -bottom-1 -right-1 block size-3 rounded-full border-2 border-white bg-blue-500 shadow-sm animate-pulse"></span>
